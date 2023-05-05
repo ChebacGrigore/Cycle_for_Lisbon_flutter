9f7e24da201c252f8c062b4860d9c501e683a230
Mubarak Ibrahim <40618838+codeswot@users.noreply.github.com>
2023-05-05 07:02:19 +0100

    events feed

--- lib/view/screens/feed/all_feed.dart
+++ lib/view/screens/feed/all_feed.dart
@@ -1,4 +1,5 @@
 import 'package:cfl/shared/shared.dart';
+import 'package:cfl/view/screens/feed/single_event.dart';
 import 'package:cfl/view/screens/feed/single_news_feed.dart';
 import 'package:cfl/view/styles/assets.dart';
 import 'package:cfl/view/styles/colors.dart';
@@ -164,6 +165,113 @@ class EventsFeed extends ConsumerStatefulWidget {
 class _EventsFeedState extends ConsumerState<EventsFeed> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: 15,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {
+            context.push(const SingleEventFeed());
+          },
+          child: Container(
+            height: 420,
+            width: double.infinity,
+            margin: const EdgeInsets.only(bottom: 16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(
+                color: AppColors.greyish,
+                width: 0.2,
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.asset(
+                    AppAssets.onboarding2,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'March 13, 2023',
+                        style: GoogleFonts.dmSans(
+                          fontSize: 12,
+                          color: AppColors.primaryColor.withOpacity(0.60),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        'Event Title',
+                        style: GoogleFonts.dmSans(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.primaryColor,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lorem pellentesque.',
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 4,
+                        style: GoogleFonts.dmSans(
+                          fontSize: 14,
+                          color: AppColors.primaryColor,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(Icons.access_time,
+                                  color: AppColors.accentColor),
+                              const SizedBox(width: 6),
+                              Text(
+                                '12:00 pm',
+                                style: GoogleFonts.dmSans(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(width: 17),
+                          Row(
+                            children: [
+                              const Icon(Icons.location_on_outlined,
+                                  color: AppColors.accentColor),
+                              const SizedBox(width: 6),
+                              Text(
+                                'Elgin St. Celina, Delaware',
+                                style: GoogleFonts.dmSans(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
--- lib/view/screens/feed/single_event.dart
+++ lib/view/screens/feed/single_event.dart
@@ -1,17 +1,197 @@
+import 'package:cfl/view/styles/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SingleEventFeed extends ConsumerStatefulWidget {
   const SingleEventFeed({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _SingleFeedScreenState();
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _SingleFeedScreenState();
 }
 
 class _SingleFeedScreenState extends ConsumerState<SingleEventFeed> {
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            automaticallyImplyLeading: true,
+            iconTheme: const IconThemeData(color: AppColors.white),
+            centerTitle: true,
+            stretch: true,
+            floating: true,
+            pinned: true,
+            expandedHeight: 250,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    alignment: Alignment.bottomCenter,
+                    image: AssetImage(
+                      AppAssets.onboarding3,
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Container(
+                  color: AppColors.black.withOpacity(0.20),
+                ),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Event Title',
+                        style: GoogleFonts.dmSans(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 22,
+                            color: AppColors.primaryColor),
+                      ),
+                      const SizedBox(height: 34),
+                      Row(
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(Icons.calendar_today_rounded,
+                                  color: AppColors.accentColor),
+                              const SizedBox(width: 6),
+                              Text(
+                                'March 13, 2023',
+                                style: GoogleFonts.dmSans(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(width: 17),
+                          Row(
+                            children: [
+                              const Icon(Icons.access_time,
+                                  color: AppColors.accentColor),
+                              const SizedBox(width: 6),
+                              Text(
+                                '12:00 pm',
+                                style: GoogleFonts.dmSans(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 14),
+                      Row(
+                        children: [
+                          const Icon(Icons.location_on_outlined,
+                              color: AppColors.accentColor),
+                          const SizedBox(width: 6),
+                          Text(
+                            'Elgin St. Celina, Delaware',
+                            style: GoogleFonts.dmSans(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Divider(),
+                      const SizedBox(height: 32),
+                      const SizedBox(height: 16),
+                      Text(
+                        'Lorem ipsum dolor sit amet consectetur. Aliquam vulputate massa id lacus gravida enim pretium sit. Sollicitudin fermentum duis ullamcorper gravida enim phasellus consectetur. Sapien sed varius fermentum dictumst varius pellentesque. Dui aliquam feugiat enim lectus.  Maecenas magna orci ut sit ultricies. Imperdiet neque libero at euismod. Tincidunt ut ac nibh amet posuere non nisl. Enim at dui in et ullamcorper. Risus tempus rhoncus tristique aliquam interdum. Elit mattis consectetur ullamcorper consectetur feugiat tempor aliquam sed tortor. Pellentesque vel cras nunc quis volutpat dictumst mauris. Scelerisque leo id eu nunc pretium viverra. Ornare id diam sagittis in ornare hendrerit dolor leo. Nec amet non mauris tincidunt mauris eget mauris.',
+                        style: GoogleFonts.dmSans(
+                            fontSize: 14,
+                            height: 2,
+                            color: AppColors.primaryColor),
+                      ),
+                      const SizedBox(height: 32),
+                      Text(
+                        'March 13, 2023',
+                        style: GoogleFonts.dmSans(
+                          color: AppColors.primaryColor.withOpacity(0.60),
+                          fontSize: 12,
+                        ),
+                      ),
+                      const SizedBox(height: 42),
+                      const Divider(),
+                      const SizedBox(height: 22),
+                      Row(
+                        children: [
+                          Text(
+                            'Share: ',
+                            style: GoogleFonts.dmSans(
+                              color: AppColors.primaryColor,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18,
+                            ),
+                          ),
+                          const SizedBox(width: 22),
+                          // TODO: use icons
+                          IconButton(
+                            color: AppColors.accentColor,
+                            onPressed: () {},
+                            icon: const Icon(Icons.facebook),
+                          ),
+                          IconButton(
+                            color: AppColors.accentColor,
+                            onPressed: () {},
+                            icon: const Icon(Icons.bookmark_outline_rounded),
+                          ),
+                          IconButton(
+                            color: AppColors.accentColor,
+                            onPressed: () {},
+                            icon: const Icon(Icons.dataset_linked_rounded),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 22),
+                      const Divider(),
+                      const SizedBox(height: 42),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton.icon(
+                            onPressed: () {},
+                            label: Text(
+                              'Back to All',
+                              style: GoogleFonts.dmSans(
+                                  color: AppColors.primaryColor),
+                            ),
+                            icon: const Icon(Icons.arrow_back,
+                                color: AppColors.primaryColor),
+                          ),
+                          TextButton.icon(
+                            onPressed: () {},
+                            icon: Text(
+                              'Next Event',
+                              style: GoogleFonts.dmSans(
+                                  color: AppColors.primaryColor),
+                            ),
+                            label: const Icon(Icons.arrow_forward,
+                                color: AppColors.primaryColor),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
