c62003cd854d4dbb4f4c0665f7c479b732c87e27
Mubarak Ibrahim <40618838+codeswot@users.noreply.github.com>
2023-05-05 06:48:37 +0100

    news feed

--- lib/view/screens/feed/single_news_feed.dart
+++ lib/view/screens/feed/single_news_feed.dart
@@ -2,6 +2,7 @@ import 'package:cfl/view/styles/assets.dart';
 import 'package:cfl/view/styles/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SingleNewsFeed extends ConsumerStatefulWidget {
   const SingleNewsFeed({super.key});
@@ -39,6 +40,103 @@ class _SingleNewsFeedState extends ConsumerState<SingleNewsFeed> {
                   color: AppColors.black.withOpacity(0.20),
                 ),
               ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'News Title',
+                        style: GoogleFonts.dmSans(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 22,
+                            color: AppColors.primaryColor),
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        'Lorem ipsum dolor sit amet consectetur. Aliquam vulputate massa id lacus gravida enim pretium sit. Sollicitudin fermentum duis ullamcorper gravida enim phasellus consectetur. Sapien sed varius fermentum dictumst varius pellentesque. Dui aliquam feugiat enim lectus.  Maecenas magna orci ut sit ultricies. Imperdiet neque libero at euismod. Tincidunt ut ac nibh amet posuere non nisl. Enim at dui in et ullamcorper. Risus tempus rhoncus tristique aliquam interdum. Elit mattis consectetur ullamcorper consectetur feugiat tempor aliquam sed tortor. Pellentesque vel cras nunc quis volutpat dictumst mauris. Scelerisque leo id eu nunc pretium viverra. Ornare id diam sagittis in ornare hendrerit dolor leo. Nec amet non mauris tincidunt mauris eget mauris.',
+                        style: GoogleFonts.dmSans(
+                            fontSize: 14,
+                            height: 2,
+                            color: AppColors.primaryColor),
+                      ),
+                      const SizedBox(height: 32),
+                      Text(
+                        'March 13, 2023',
+                        style: GoogleFonts.dmSans(
+                          color: AppColors.primaryColor.withOpacity(0.60),
+                          fontSize: 12,
+                        ),
+                      ),
+                      const SizedBox(height: 42),
+                      const Divider(),
+                      const SizedBox(height: 22),
+                      Row(
+                        children: [
+                          Text(
+                            'Share: ',
+                            style: GoogleFonts.dmSans(
+                              color: AppColors.primaryColor,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18,
+                            ),
+                          ),
+                          const SizedBox(width: 22),
+                          // TODO: use icons
+                          IconButton(
+                            color: AppColors.accentColor,
+                            onPressed: () {},
+                            icon: const Icon(Icons.facebook),
+                          ),
+                          IconButton(
+                            color: AppColors.accentColor,
+                            onPressed: () {},
+                            icon: const Icon(Icons.bookmark_outline_rounded),
+                          ),
+                          IconButton(
+                            color: AppColors.accentColor,
+                            onPressed: () {},
+                            icon: const Icon(Icons.dataset_linked_rounded),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 22),
+                      const Divider(),
+                      const SizedBox(height: 42),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton.icon(
+                            onPressed: () {},
+                            label: Text(
+                              'Back to All',
+                              style: GoogleFonts.dmSans(
+                                  color: AppColors.primaryColor),
+                            ),
+                            icon: const Icon(Icons.arrow_back,
+                                color: AppColors.primaryColor),
+                          ),
+                          TextButton.icon(
+                            onPressed: () {},
+                            icon: Text(
+                              'Next Article',
+                              style: GoogleFonts.dmSans(
+                                  color: AppColors.primaryColor),
+                            ),
+                            label: const Icon(Icons.arrow_forward,
+                                color: AppColors.primaryColor),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ],
